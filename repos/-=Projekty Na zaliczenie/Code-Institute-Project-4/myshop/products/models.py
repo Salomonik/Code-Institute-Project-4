a3bdaa680a90ba96619a4389f5cb438b0527c6ab
Salomonik <salomonik@gmail.com>
2024-10-19 11:41:26 +0100

    add products model

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/models.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Product model to store product details
+class Product(models.Model):
+    # The name of the product, maximum length is 255 characters
+    name = models.CharField(max_length=255)
+    
+    # A brief description of the product
+    description = models.TextField()
+    
+    # Price of the product, defined as a decimal with two decimal places
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
