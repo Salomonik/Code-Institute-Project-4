66dfaa030e70edefc0f464a664c0f337a002e0b9
Salomonik <salomonik@gmail.com>
2024-10-19 14:45:12 +0100

    add about html content

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Widok strony głównej
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
