fda336f9b375910529a5aa970c7c6310f92c4730
Salomonik <salomonik@gmail.com>
2024-10-19 11:48:05 +0100

    add view for products

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+# View to list all products on the main page
+def product_list(request):
+    # Query to get all products from the database
+    products = Product.objects.all()
+    
+    # Return the list of products to be rendered in the template
+    return render(request, 'products/product_list.html', {'products': products})
