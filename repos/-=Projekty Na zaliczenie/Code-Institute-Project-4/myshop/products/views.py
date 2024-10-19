72e682bfe42af7c1888286bbc560376c7d289de2
Salomonik <salomonik@gmail.com>
2024-10-19 13:11:50 +0100

    update views.py for category

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # View to list all products on the main page
 def product_list(request):
@@ -16,3 +16,13 @@ def product_detail(request, product_id):
     
     # Render the product detail template and pass the product object
     return render(request, 'products/product_detail.html', {'product': product})
+
+# View to list products in a specific category
+def category_products(request, category_slug):
+    # Get the category based on the slug
+    category = get_object_or_404(Category, slug=category_slug)
+    
+    # Get all products that belong to this category
+    products = category.products.all()
+    
+    return render(request, 'products/category_products.html', {'category': category, 'products': products})
