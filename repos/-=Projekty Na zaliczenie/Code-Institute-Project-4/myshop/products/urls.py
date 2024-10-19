b3e35f493d6775d0bbf70f3445c611d7905af9c1
Salomonik <salomonik@gmail.com>
2024-10-19 13:12:45 +0100

    update product urls for category

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/urls.py
@@ -4,5 +4,7 @@ from . import views
 # Defining URL patterns for the products app
 urlpatterns = [
     path('', views.product_list, name='product_list'),  # Default route for listing products
-    path('<int:product_id>/', views.product_detail, name='product_detail') # route for product details
+    path('<int:product_id>/', views.product_detail, name='product_detail'), # route for product details
+    # URL to filter products by category
+    path('category/<slug:category_slug>/', views.category_products, name='category_products'),
 ]
