3b8873b77dc8c0ec79338c5f81e76f336eae4b7e
Salomonik <salomonik@gmail.com>
2024-10-19 12:06:11 +0100

    add route for product details

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/urls.py
@@ -4,4 +4,5 @@ from . import views
 # Defining URL patterns for the products app
 urlpatterns = [
     path('', views.product_list, name='product_list'),  # Default route for listing products
+    path('<int:product_id>/', views.product_detail, name='product_detail') # route for product details
 ]
