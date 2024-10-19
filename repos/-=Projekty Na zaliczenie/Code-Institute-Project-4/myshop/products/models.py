d641e2db82ad1a6f3f4ed9705bff37299369727b
Salomonik <salomonik@gmail.com>
2024-10-19 11:42:15 +0100

    add rest of products models and representation string

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/models.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/models.py
@@ -13,4 +13,18 @@ class Product(models.Model):
     # Price of the product, defined as a decimal with two decimal places
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
+     # Stock represents how many items are available in the store
+    stock = models.IntegerField()
     
+    # An optional image for the product, images are stored in 'product_images/' directory
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    
+    # Automatically sets the date when the product is added
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    # Automatically updates the date when the product is updated
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # String representation of the product
+    def __str__(self):
+        return self.name
