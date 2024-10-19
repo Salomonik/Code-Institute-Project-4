508d00ff864eb64538f040ab3018c22b5d17e3b0
Salomonik <salomonik@gmail.com>
2024-10-19 12:05:09 +0100

    add views for product details

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # View to list all products on the main page
@@ -8,3 +8,11 @@ def product_list(request):
     
     # Return the list of products to be rendered in the template
     return render(request, 'products/product_list.html', {'products': products})
+
+# View to display product details
+def product_detail(request, product_id):
+    # Fetch the product based on its ID, or return 404 if not found
+    product = get_object_or_404(Product, id=product_id)
+    
+    # Render the product detail template and pass the product object
+    return render(request, 'products/product_detail.html', {'product': product})
