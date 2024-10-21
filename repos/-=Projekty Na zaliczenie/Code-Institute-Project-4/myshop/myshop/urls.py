4a2dd543a736f2833e63704e32f14b5ff0412be1
Salomonik <salomonik@gmail.com>
2024-10-21 14:33:56 +0100

    add path for login/logout in urls.py

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-
-    
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),  
 ]
 
 if settings.DEBUG:
