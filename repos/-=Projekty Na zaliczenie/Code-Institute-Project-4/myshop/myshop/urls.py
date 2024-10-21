2147138f46a240e6e75132a3f0da073b7dde6289
Salomonik <salomonik@gmail.com>
2024-10-21 14:55:44 +0100

    add path for register

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),  
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
 if settings.DEBUG:
