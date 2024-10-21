374556e1c943ac56b568474eb90abb57a32cf8c3
Salomonik <salomonik@gmail.com>
2024-10-21 14:33:23 +0100

    add off canvas for login and register

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +28,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
+    
 ]
 
 if settings.DEBUG:
