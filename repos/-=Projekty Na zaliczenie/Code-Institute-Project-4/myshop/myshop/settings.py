1d1bf3a9e9ea64e2f44c3ec9db0ad611afaa8ea9
Salomonik <salomonik@gmail.com>
2024-10-21 14:58:58 +0100

    add path for pass reset

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/settings.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',   'products',
+    'django.contrib.staticfiles',
+    'products',
     'checkout',
     'accounts',
 
