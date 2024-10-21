ca001f64e927e8afe906e75dc4353093a435f87e
Salomonik <salomonik@gmail.com>
2024-10-21 14:55:13 +0100

    add view for register

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Widok strony głównej
 def home(request):
@@ -9,3 +10,13 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Przekierowanie do logowania po rejestracji
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
