3d4591c3851142fb6880aa0dafe55af7f737e780
Salomonik <salomonik@gmail.com>
2024-10-22 19:41:56 +0100

    add cart model in cart/models

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/models.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/models.py
@@ -3,6 +3,14 @@ from products.models import Product
 from django.contrib.auth.models import User
 from django.conf import settings
 
+# Define the Cart model
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart ({self.user.username})"
+
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Optional for anonymous users
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
