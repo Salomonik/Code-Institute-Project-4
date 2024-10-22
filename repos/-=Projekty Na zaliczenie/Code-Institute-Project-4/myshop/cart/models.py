a6ce246674e42e6d8351dfccfce87af791475093
Salomonik <salomonik@gmail.com>
2024-10-22 18:59:04 +0100

    add cart models

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/models.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Optional for anonymous users
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity})"
+
+    def get_total_price(self):
+        return self.quantity * self.product.price
