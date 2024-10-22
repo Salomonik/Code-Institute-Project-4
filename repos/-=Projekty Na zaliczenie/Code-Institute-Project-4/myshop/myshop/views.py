495fcc54e9b9631b90bb4b07edfceb0d7b106794
Salomonik <salomonik@gmail.com>
2024-10-22 17:27:25 +0100

    add view for login

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/myshop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.contrib.auth import login, get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, get_user_model, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib import messages  # For displaying messages to the user
 from accounts.forms import UserRegisterForm  # Import the form from forms.py
@@ -44,3 +44,18 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have been successfully logged out.')
     return redirect('home')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                    return JsonResponse({'success': True, 'message': 'Login successful!'})
+                return redirect('home')  # Redirect to home on success
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+    return render(request, 'login.html', {'form': AuthenticationForm()})
