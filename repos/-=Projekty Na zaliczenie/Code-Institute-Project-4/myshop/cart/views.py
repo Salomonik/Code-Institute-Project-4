f1ee193dfaf22eb60bc20f1424eff68467b4f2f1
Salomonik <salomonik@gmail.com>
2024-10-22 19:00:14 +0100

    add add_to_cart view

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from .models import CartItem
+from .forms import CartAddProductForm
 
-# Create your views here.
+# View to add an item to the cart
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        quantity = form.cleaned_data['quantity']
+        user = request.user if request.user.is_authenticated else None
+        cart_item, created = CartItem.objects.get_or_create(
+            product=product, user=user, defaults={'quantity': quantity}
+        )
+        if not created:
+            cart_item.quantity += quantity
+        cart_item.save()
+
+    return redirect('cart:cart_detail')
