d309dcd39bb33d3cdb4d1db3066a9ba5daea70ce
Salomonik <salomonik@gmail.com>
2024-10-22 19:01:09 +0100

    add remove_from_cart in view

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/views.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/cart/views.py
@@ -25,3 +25,10 @@ def cart_detail(request):
     cart_items = CartItem.objects.filter(user=request.user if request.user.is_authenticated else None)
     total_price = sum(item.get_total_price() for item in cart_items)
     return render(request, 'cart/cart_detail.html', {'cart_items': cart_items, 'total_price': total_price})
+
+# View to remove an item from the cart
+def remove_from_cart(request, product_id):
+    user = request.user if request.user.is_authenticated else None
+    cart_item = get_object_or_404(CartItem, product_id=product_id, user=user)
+    cart_item.delete()
+    return redirect('cart:cart_detail')
