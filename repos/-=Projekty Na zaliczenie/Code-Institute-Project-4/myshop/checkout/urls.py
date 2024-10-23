46d714d28a423d112ab611b80612ac94aba90700
Salomonik <salomonik@gmail.com>
2024-10-23 19:18:23 +0100

    add checkout in urls

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/urls.py
@@ -5,4 +5,6 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('', views.checkout_page, name='checkout_page'),  # Main checkout page
+    path('checkout/', views.checkout, name='checkout'),
+    path('checkout/success/', views.checkout_success, name='checkout_success'),
 ]
