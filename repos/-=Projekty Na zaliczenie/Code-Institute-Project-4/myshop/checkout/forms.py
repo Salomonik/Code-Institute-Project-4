7a3efa71f3b83a5e17b0e4ce05f3bef8863d698b
Salomonik <salomonik@gmail.com>
2024-10-23 20:13:51 +0100

    update checkout form

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/forms.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/forms.py
@@ -1,7 +1,40 @@
 from django import forms
+from django_countries.fields import CountryField
+from phonenumber_field.formfields import PhoneNumberField
 
 class CheckoutForm(forms.Form):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    address = forms.CharField(widget=forms.Textarea, label="Shipping Address")
+    full_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': 'Full Name',
+        'class': 'form-control'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Email Address',
+        'class': 'form-control'
+    }))
+    phone_number = PhoneNumberField(widget=forms.TextInput(attrs={
+        'placeholder': 'Phone Number',
+        'class': 'form-control'
+    }))
+    street_address1 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'placeholder': 'Street Address 1',
+        'class': 'form-control'
+    }))
+    street_address2 = forms.CharField(required=False, max_length=255, widget=forms.TextInput(attrs={
+        'placeholder': 'Street Address 2',
+        'class': 'form-control'
+    }))
+    city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': 'Town or City',
+        'class': 'form-control'
+    }))
+    county = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': 'County',
+        'class': 'form-control'
+    }))
+    postal_code = forms.CharField(max_length=20, widget=forms.TextInput(attrs={
+        'placeholder': 'Postal Code',
+        'class': 'form-control'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(widget=forms.Select(attrs={
+        'class': 'form-control'
+    }))
