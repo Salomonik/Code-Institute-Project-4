d37df5af02060c530474f940966f6a371236d842
Salomonik <salomonik@gmail.com>
2024-10-23 19:10:21 +0100

    add models for checkout

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/models.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.id} - User: {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} - Quantity: {self.quantity}"
