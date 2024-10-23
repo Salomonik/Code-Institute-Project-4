a095299b969a39239317855c1ea9bc6b1ac27288
Salomonik <salomonik@gmail.com>
2024-10-23 21:53:44 +0100

    add stripe js in checkout.html

--- repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/urls.py
+++ repos/-=Projekty Na zaliczenie/Code-Institute-Project-4/myshop/checkout/urls.py
@@ -6,4 +6,5 @@ app_name = 'checkout'
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout/success/', views.checkout_success, name='checkout_success'),
+    path('checkout/cancel/', views.checkout_cancel, name='checkout_cancel'),  # Cancel page in case of payment failure
 ]
